003443225de1c75fca50faf9eb4b78f7165c4ce3
Pawan Kumar <naveen.p@northeastern.edu>
2024-02-12 21:55:54 -0500

    Activity (Feb 8)

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,22 @@
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Profile from './components/profile'
+import PackingList from './components/props_item'
+import Gallery from './components/gallery'
+import Bio from './qcomps/bios'
+import MyComp from './qcomps/firstcomp'
+import TodoList from './qcomps/todos'
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+        <Gallery/>
+        <Bio/>
+        <MyComp/>
+        <Profile/>
+        <TodoList/>
+        <Gallery/>
+        <PackingList/>
     </div>
   )
 }
